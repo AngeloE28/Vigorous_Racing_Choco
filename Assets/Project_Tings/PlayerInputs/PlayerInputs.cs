a23c0c79d9d5b4b2498035e1b624ebf9f63e3aef
AngeloE28 <66254064+AngeloE28@users.noreply.github.com>
2021-10-08 02:11:33 +1100

    Flip and fixed wheel animation

--- Assets/Project_Tings/PlayerInputs/PlayerInputs.cs
+++ Assets/Project_Tings/PlayerInputs/PlayerInputs.cs
@@ -9,7 +9,10 @@ public class PlayerInputs : MonoBehaviour
     // Inputs
     private PlayerInputActions playerInputActions;
 
+    [SerializeField] private PlayerCamera cameraController;
+
     // Player colliders and controllers
+    [Header("Colliders and Controller")]
     [SerializeField] private Rigidbody carControllerRB;
     [SerializeField] private SphereCollider carControllerCollider;
 
@@ -21,8 +24,15 @@ public class PlayerInputs : MonoBehaviour
     [SerializeField] private float turnStrengthOnGround, turnStrengthInAir; // Maximum turn angles
     public float smoothCarRotationVal = 15.0f; // For smooth rotations, when driving on slopes
 
+    [Header("Boost Values")]
+    [SerializeField] private float flipBoostAmount = 500.0f;
+
     // Inputs
     private float speedInput, turnInput;
+
+    // Flip boost
+    private float spinTotal;
+    private bool allowFlipBoost = false;
 
     // Is car grounded
     private bool isGrounded = true; 
@@ -38,12 +48,13 @@ public class PlayerInputs : MonoBehaviour
     [SerializeField] private Transform frontLeftWheelModel;
     [SerializeField] private Transform frontRightWheelModel;
     [SerializeField] private Transform[] backWheels;
+    private float wheelSpinDirection;
 
 
     private void Awake()
     {
         playerInputActions = new PlayerInputActions();
-        playerInputActions.Player.Enable();    
+        playerInputActions.Player.Enable();        
     }
 
 
@@ -77,11 +88,13 @@ public class PlayerInputs : MonoBehaviour
         if (inputAccelerate > 0)
         {
             speedInput = inputAccelerate * forwardAccel * accelMultiplier;
+            wheelSpinDirection = 1;
         }
         // Reversing
         else if (inputAccelerate < 0)
         {
             speedInput = inputAccelerate * reverseAccel * accelMultiplier;
+            wheelSpinDirection = -1;
         }
 
         // Turn input
@@ -95,9 +108,9 @@ public class PlayerInputs : MonoBehaviour
                                                   0.0f));
         }
 
+        Vector2 stickInput = playerInputActions.Player.Move.ReadValue<Vector2>();
         if (!isGrounded)
         {
-            Vector2 stickInput = playerInputActions.Player.Move.ReadValue<Vector2>();
             transform.Rotate(Vector3.right * stickInput.y * turnStrengthInAir * Time.deltaTime);
 
             transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles + new Vector3(0.0f,
@@ -105,34 +118,16 @@ public class PlayerInputs : MonoBehaviour
                                                   0.0f));
         }
         
+        // Apply a boost after spinning around a certain amount
+        FlipBoost(stickInput);        
+        
         // Procedurally Animate the wheels
-        WheelTurns(turnInput, inputAccelerate, maxWheelTurnAngle, turnStrengthOnGround);
+        WheelTurns(turnInput, maxWheelTurnAngle);
 
         // Car follows the carController thats moving
         transform.position = carControllerRB.transform.position;        
     }
-
-    private void WheelTurns(float turnInput, float accelerateInput, float turnAngle, float turnStrength)
-    {
-        // Turn both wheels in the direction the car is turning
-        frontLeftWheel.localRotation = Quaternion.Euler(frontLeftWheel.localRotation.eulerAngles.x,
-                                                        (turnInput * turnAngle),
-                                                        frontLeftWheel.localRotation.eulerAngles.z);
-
-        frontRightWheel.localRotation = Quaternion.Euler(frontRightWheel.localRotation.eulerAngles.x,
-                                                        (turnInput * turnAngle),
-                                                        frontRightWheel.localRotation.eulerAngles.z);
-
-        float turnMultiplier = 10.0f;
-        // Spin the wheel back or forward
-        frontLeftWheelModel.Rotate(-Vector3.up * accelerateInput * turnStrength * turnMultiplier * Time.deltaTime);
-        frontRightWheelModel.Rotate(Vector3.up * accelerateInput * turnStrength * turnMultiplier * Time.deltaTime);
-        foreach (Transform t in backWheels)
-        {
-            t.Rotate(Vector3.right * accelerateInput * turnStrength * turnMultiplier * Time.deltaTime);
-        }
-    }
-
+      
     private void CarEngine()
     {
         isGrounded = false;
@@ -176,6 +171,82 @@ public class PlayerInputs : MonoBehaviour
             carControllerRB.drag = dragInAir;
             carControllerRB.AddForce(Vector3.up * -gravityForce * gravityMultiplier);
         }
+    }
+
+    private void WheelTurns(float turnInput, float turnAngle)
+    {
+        // Turn both wheels in the direction the car is turning
+        frontLeftWheel.localRotation = Quaternion.Euler(frontLeftWheel.localRotation.eulerAngles.x,
+                                                        (turnInput * turnAngle),
+                                                        frontLeftWheel.localRotation.eulerAngles.z);
+
+        frontRightWheel.localRotation = Quaternion.Euler(frontRightWheel.localRotation.eulerAngles.x,
+                                                        (turnInput * turnAngle),
+                                                        frontRightWheel.localRotation.eulerAngles.z);
+
+        float turnMultiplier = 100.0f;
+        // Spin the wheel back or forward
+        frontLeftWheelModel.Rotate(-Vector3.up * carControllerRB.velocity.magnitude * turnMultiplier * wheelSpinDirection * Time.deltaTime);
+        frontRightWheelModel.Rotate(Vector3.up * carControllerRB.velocity.magnitude * turnMultiplier * wheelSpinDirection * Time.deltaTime);
+        foreach (Transform t in backWheels)
+        {
+            t.Rotate(Vector3.right * carControllerRB.velocity.magnitude * turnMultiplier * wheelSpinDirection * Time.deltaTime);
+        }
+    }
+
+    private void FlipBoost(Vector2 stickInput)
+    {
+        // Provide a boost after rotating a certain amount        
+        float boostFlipRequirement = 130.0f;        
+        
+        // Increment the amount of spin
+        if (!isGrounded)                   
+            spinTotal += stickInput.magnitude;
+
+        // Check if player cna boost or not
+        if (spinTotal > boostFlipRequirement)
+            allowFlipBoost = true;
+        else
+            allowFlipBoost = false;        
+
+        // Execute an action if player can boost or not
+        if (allowFlipBoost)
+        {                       
+            if (isGrounded)
+            {                
+                StartCoroutine(ResetFlipBoost());  
+            }
+        }
+        else
+        {
+            if (isGrounded)
+            {
+                if (spinTotal > 0 && spinTotal < boostFlipRequirement)
+                    spinTotal = 0.0f;
+            }
+        }
+    }
+
+    private IEnumerator ResetFlipBoost()
+    {
+        float resetFlipBoostTime = 0.1f;
+        float resetCamTime = 0.3f;        
+
+        yield return new WaitForSeconds(resetFlipBoostTime);
+
+        // Boost and boost effect
+        cameraController.SetCamPos(true);
+        carControllerRB.AddForce(transform.forward * flipBoostAmount, ForceMode.Impulse);
+
+        // Reset values
+        allowFlipBoost = false;
+        spinTotal = 0.0f;
+        Invoke("ResetCamPos", resetCamTime);
+    }
+
+    private void ResetCamPos()
+    {
+        cameraController.SetCamPos(false);
     }
 
     public bool GetIsGroundedState()
--- Assets/Project_Tings/Scripts/Player/PlayerCamera.cs
+++ Assets/Project_Tings/Scripts/Player/PlayerCamera.cs
@@ -7,7 +7,15 @@ public class PlayerCamera : MonoBehaviour
     [SerializeField] private Transform player;
     [SerializeField] private PlayerInputs carController;
 
+    [Header("Camera Positions")]
+    [SerializeField] private float originalFOV = 40;
+    [SerializeField] private float boostFOV = 60;
+    [SerializeField] private Camera playerCam;    
+    private bool camBoostPos = false;
+    private float camPosIncrement = 0.15f;
+
     // Smooth values
+    [Header("Camera Rotations")]
     private float smoothVal = 3.0f;
     [SerializeField] private float groundSmoothVal = 3.0f;
     [SerializeField] private float airSmoothVal = 0.1f;
@@ -18,7 +26,20 @@ public class PlayerCamera : MonoBehaviour
         if (carController.GetIsGroundedState())
             smoothVal = groundSmoothVal;
         if (!carController.GetIsGroundedState())
-            smoothVal = airSmoothVal;        
+            smoothVal = airSmoothVal;
+
+        // Move the camera position if the player is boosting or finished boosting
+        float boostFOVMultiplier = 5.0f;
+        if (camBoostPos)
+        {
+            if (playerCam.fieldOfView != boostFOV && playerCam.fieldOfView < boostFOV)
+                playerCam.fieldOfView += camPosIncrement * boostFOVMultiplier;
+        }
+        else
+        {
+            if (playerCam.fieldOfView != originalFOV && playerCam.fieldOfView > originalFOV)
+                playerCam.fieldOfView -= camPosIncrement;
+        }
     }
 
     private void LateUpdate()
@@ -26,5 +47,11 @@ public class PlayerCamera : MonoBehaviour
         // Move and rotate the camera pivot to be the same as the player
         transform.position = player.position;
         transform.rotation = Quaternion.Slerp(transform.rotation, player.rotation, smoothVal * Time.deltaTime);
+    }
+
+    public void SetCamPos(bool boost)
+    {
+        // Did the player boost or not?
+        camBoostPos = boost;
     }
 }
